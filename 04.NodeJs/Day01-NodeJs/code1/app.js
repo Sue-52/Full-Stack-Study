b4446527b836ef3ebf24edc23a62bf85c2ef8394
Sue-52 <1219243947@qq.com>
2021-09-25 08:43:01 +0800

    submit

--- 04.NodeJs/Day01-NodeJs/code1/app.js
+++ 04.NodeJs/Day01-NodeJs/code1/app.js
@@ -1,5 +1,15 @@
 const fs = require("fs")
 
+fs.writeFile(
+  __dirname + "/content.txt",
+  "sue",
+  { encoding: "utf-8" },
+  (error, data) => {
+    if (error) throw err;
+    console.log(data);
+  }
+);
+
 fs.readFile(__dirname + "/content.txt", { enconding: "utf-8", flag: "r" }, (error, data) => {
   if (error) {
     console.log(error.message)
